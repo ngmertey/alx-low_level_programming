7511b480bb70081ac4ac750b9415d447b9ca3785
joshua <ngmerteyjoshua360@gmail.com>
2023-10-06 18:03:19 -0700

    printing single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program to print single digits numbers of base 10
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar('0');
+	putchar('\n');
+
+	return (0);
+}
