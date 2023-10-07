46f629d1d76897db959fa12acd82c680e0948e98
joshua <ngmerteyjoshua360@gmail.com>
2023-10-06 18:15:27 -0700

    printing single digit numbers of base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,18 @@
 
 /**
  * main - Entry point
- * Description: A program to print single digits numbers of base 10
+ * Description: A program to print numbers from 0 to 9
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	putchar('0');
-	putchar('\n');
+	int i;
 
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	putchar('\n');
 	return (0);
 }
