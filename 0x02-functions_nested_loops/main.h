be9eee4f5eb89409e24d77475f93f777c38e620c
joshua <ngmerteyjoshua360@gmail.com>
2023-10-10 12:57:01 -0700

    This is a header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Joshua Ngmertey
+ * Description: Header file containing declarations for all the functions going
+ * 	to be used in this project 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
