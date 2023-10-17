d0bc97e57eb0afb862572478c35d0f69c948d714
joshua <ngmerteyjoshua360@gmail.com>
2023-10-16 17:30:21 -0700

    prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,24 @@
 
 void puts_half(char *str)
 {
-	int i = 0, len = 0;
+	int i = 0, len = 0, n;
 
 	while (str[i++])
 	{
 		len++;
 	}
 
-	for (i = 5; i < len; i++)
+	if ((len % 2) == 0)
+	{
+		n = len / 2;
+	}
+
+	else
+	{
+		n = (len + 1) / 2;
+	}
+
+	for (i = n; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
