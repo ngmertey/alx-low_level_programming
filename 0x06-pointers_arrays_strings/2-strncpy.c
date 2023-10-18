03734c3b46d5c75dca55fb48405f3b9eab837c9c
joshua <ngmerteyjoshua360@gmail.com>
2023-10-18 14:38:42 -0700

    Copies at most an inputted number of bytes from string src into dest.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *answer = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+
+	return (answer);
+}
