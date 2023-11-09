7c99b3c9c4beea7546661daf1043df14701a0025
joshua <ngmerteyjoshua360@gmail.com>
2023-11-08 18:15:39 -0800

    implementation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,3 +1,4 @@
+#include "3-calc.h"
 #include <stdlib.h>
 
 /**
@@ -20,12 +21,8 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if (*s == *(ops[i].op) && *(s + 1) == '\0')
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	printf("Error\n");
-	exit(99);
+
+	return (ops[i].f);
 }
